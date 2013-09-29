75c636ec8782bfef76ebe45228af277b61abada4
Rev. Johnny Healey <rev.null@gmail.com>
2013-09-29 12:41:17 -0400

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name="tweeter",
+    version="0.0.1",
+    description="A program to turn tweets into music",
+    author="Rev. Johnny Healey",
+    author_email="rev.null@gmail.com",
+    packages = ["tweeter"]
+)
